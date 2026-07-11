42edda4b5a44a1b75d91fdd73fd0b5b765943a77
asifmalaika513 <asifmalaika513@gmail.com>
2026-07-11 08:14:43 -0700

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,9 +1,10 @@
-#arithmetic oprators
-a=5
-b=3
+#relational oprators
+a=50
+b=20
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a#b)=reminder
+print(a==b)False
+print(a!=b)True
+print(a>=b)True
+print(a>b)True
+print(a<=b)False
+print(a<b)False
